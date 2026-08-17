7ba85229a72e55eeb0eef6a1c3e0899cbc4d6ae0
Johnson I <jhnikome@gmail.com>
2026-08-17 18:45:27 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# My-portfolio
+This is my Portfolio website of all projects I have worked on so far.
+(1) It consist of an online grocery shopping app.
+(2) An Insurance website.
+(3) A Demo banking app.
+(4) A restaurant website.
---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+// sticky navigation
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyHead = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyHead, {
+  root: null,
+  threshold: 0.15,
+});
+headerObserver.observe(hero);
+
+// copyright
+const date = document.querySelector(".date");
+
+const now = new Date();
+const y = now.getFullYear();
+date.textContent = y;
